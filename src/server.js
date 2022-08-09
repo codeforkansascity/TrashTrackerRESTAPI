d0e8390c7659185829bf9725b2d480dc63c90eb2
lulu <lulu.cao.her@gmail.com>
2022-08-09 11:20:19 -0500

    testing http request

--- src/server.js
+++ src/server.js
@@ -1,79 +1,30 @@
-const express = require("express");
-const path = require('path');
-const fs = require("fs");
-const saveImage = require("./save_image.js");
-const exif = require("exiftool");
+const fs = require('fs');
+const html = require('../public/assets/index.js');
+// const html = fs.readFileSync('./index.html', { encoding:'utf8' });
 
-const app = express();
+/**
+ * Returns an HTML page containing an interactive Web-based
+ * tutorial. Visit the function URL to see it and learn how
+ * to build with lambda.
+ */
+exports.handler = async (event) => {
+    let modifiedHTML = dynamicForm(html,event.queryStringParameters)
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html',
+        },
+        body: modifiedHTML
+    };
+    return response;
+};
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/assets/log.txt'));
-});
-
-app.post("/", (req, res) => {
-  fs.appendFile(
-    "./public/assets/log.txt",
-    `\nNew report: \n${JSON.stringify(req.body)}\n`,
-    (err) =>
-      err
-        ? console.log(err)
-        : console.log(
-            "\nNew report: \nGeneral data are logged to ./public/assets/log.txt successfully."
-          )
-  );
-  // if (req.body.photo_url === "") {
-  //   console.log(
-  //     "User(s) have not sent a photo. No geolocation data available."
-  //   );
-  // } else {
-  //   saveImage.download(
-  //     `${req.body.photo_url}`,
-  //     "./public/assets/image.jpg",
-  //     function () {
-  //       fs.readFile(
-  //         "./public/assets/image.jpg",
-  //         ["-gpsPosition"],
-  //         function (err, data) {
-  //           if (err) throw err;
-  //           else {
-  //             console.log("Photo is stored in ./public/assets successfully.");
-  //             exif.metadata(data, function (err, metadata) {
-  //               if (err) throw err;
-  //               else {
-  //                 if (metadata.gpsPosition === undefined) {
-  //                   fs.appendFile(
-  //                     "./public/assets/log.txt",
-  //                     "GPSPosition: this photo does not contain geolocation data.\n",
-  //                     (err) =>
-  //                       err
-  //                         ? console.log(err)
-  //                         : console.log("No geolocation data are found in the photo.")
-  //                   );
-  //                 } else {
-  //                   fs.appendFile(
-  //                     "./public/assets/log.txt",
-  //                     `GPSPosition: ${metadata.gpsPosition}\n`,
-  //                     (err) =>
-  //                       err
-  //                         ? console.log(err)
-  //                         : console.log(
-  //                             "Geolocation data are extracted successfully."
-  //                           )
-  //                   );
-  //                 }
-  //               }
-  //             });
-  //           }
-  //         }
-  //       );
-  //     }
-  //   );
-  // } 
-  res.end();
-});
-
-module.exports = app;
-
+function dynamicForm(html,queryStringParameters){
+    let formres = ''
+    if(queryStringParameters){
+            Object.values(queryStringParameters).forEach(val => {
+                  formres =formres+val+' ';
+            });
+    }
+    return html.replace('{formResults}','<h4>Form Submission: '+formres+'</h4>')
+}
